82394efc35e83c01e4c1f236089a704ad7ab232f
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2019-05-27 13:14:17 -0400

    Learning How To Retart Game
    1. Learned how to share info between screens with self.manager
    
    2.This is the main thing, I made it so that on the losing screen when you want to restart the game, some the the  variables get reset, and the buttons that delete themselves get restored. In the custonbutton class you make it so that the info of each button gets stored in a dictionary, and that dictionary is added to a list. Restore_buttons is latter called to add_widgets with the info of the fallen buttons. Works perfectly.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
 from kivy.properties import ObjectProperty
 from kivy.graphics import Color, Rectangle
 from kivy.lang import Builder
@@ -21,33 +22,36 @@ guessed_letters = []
 guessed_letter = ""
 count = 0
 hangman = IDK.x.selecting_hangman()
-	
+deleted_buttons = []
 
 class CustomButton(Button,IDK): #blueprint for my special buttons that delete themselves on touch
     def on_touch_down(self, touch):
         global count
         global guessed_letters
         global guessed_letter
+        global deleted_buttons
         if self.collide_point(*touch.pos): #if touched
-            print(f"\nCustomButton.on_touch_down: text/id={self.id}")
+           # print(f"\nCustomButton.on_touch_down: text/id={self.id}")
             guessed_letter = self.id  #the guessed letter used for logic is the id of the button
             guessed_letters.append(guessed_letter) #add that letter to the list of used letters for logic
             count =  IDK.x.test_guessed_letter(guessed_letter, hangman, count) #sees if it was right or wrong and adjusts count accordingly
-            self.parent.ids.btn_rep.text = IDK.x.nitty_gritty(hangman, guessed_letter, guessed_letters) #the representation of all the letters guessed
+            self.parent.btn_rep.text = IDK.x.nitty_gritty(hangman, guessed_letter, guessed_letters) #the representation of all the letters guessed
          
-            if '_' not in self.parent.ids.btn_rep.text:
+            if '_' not in self.parent.btn_rep.text:
                 sm.current = "forth"
             
             if count == 1:
-                self.parent.ids.new_image.source = 'hangmanpic1.png'
+                self.parent.new_image.source = 'hangmanpic1.png'
             elif count == 2:
-                self.parent.ids.new_image.source = 'hangmanpic2.png'
+                self.parent.new_image.source = 'hangmanpic2.png'
             elif count == 3:
-                self.parent.ids.new_image.source = 'hangmanpic3.png'
+                self.parent.new_image.source = 'hangmanpic3.png'
             elif count == 4:
-                self.parent.ids.new_image.source = 'hangmanpic4.png'
+                self.parent.new_image.source = 'hangmanpic4.png'
                 sm.current = "fifth"
             if count != 4:
+                btn_info = {"text" : self.text, id : self.id, "pos_hint" : self.pos_hint, "size_hint": self.size_hint}
+                deleted_buttons.append(btn_info)
                 self.parent.remove_widget(self) #suicides itself
             return True    # consumed on_touch_down & stop propagation / bubbling
         return super(CustomButton, self).on_touch_down(touch)
@@ -60,10 +64,14 @@ class MainWindow(Screen):
 class SecondWindow(Screen,mh):
 	pass
 class ThirdWindow(Screen, IDK):
+	global hangman
 
 	def __init__(self, **kwargs):
 		super(ThirdWindow, self).__init__(**kwargs)
+		self.btn_rep = ""
+		self.new_image = ""
 		self.create_buttons()
+		
 
 	def create_buttons(self): #properties of each button
 		k = .65
@@ -79,11 +87,47 @@ class ThirdWindow(Screen, IDK):
 				j -= .1
 				k = 0.65
 				c = 0	
-class ForthWindow(Screen,mh):
-	pass
+		
+		self.btn_rep =((Label(id = "btn_rep", text= "Secret Word", font_size = 30, color = (1,1,1,1), size_hint =  (.3, .3), pos_hint = {'x':.325-.15,'y':.2-.15})))
+		self.add_widget(self.btn_rep)
+		self.new_image = Image(source = 'hangmanpic0.png', size_hint = (.6,.7), pos_hint = {'x':.2, 'y': .32}, allow_stretch = True)
+		self.add_widget(self.new_image)
+		
+	def restore_buttons(self): 
+		global deleted_buttons
+		for button in deleted_buttons:
+			self.add_widget(CustomButton(id = button["text"], text= button["text"], pos_hint = button["pos_hint"], size_hint = button["size_hint"]))
+			print("TEST: " , button["text"])
 
+		
+		
+class ForthWindow(Screen):
+	def test(self):
+		global guessed_letters
+		global guessed_letter 
+		global hangman  
+		global count
+		hangman = IDK.x.selecting_hangman()
+		count = 0
+		guessed_letter = ""
+		guessed_letters = []
+		s3 = self.manager.get_screen("third")
+		s3.restore_buttons()
+		
+	
 class FifthWindow(Screen):
-	pass
+	def test(self):
+		global guessed_letters
+		global guessed_letter 
+		global hangman  
+		global count
+		hangman = IDK.x.selecting_hangman()
+		count = 0
+		guessed_letter = ""
+		guessed_letters = []
+		s3 = self.manager.get_screen("third")
+		s3.restore_buttons() 
+		
 	
 sm = WindowManager()
 sm.add_widget(MainWindow())
@@ -93,6 +137,7 @@ sm.add_widget(ForthWindow())
 sm.add_widget(FifthWindow())
 
 class HangmanApp(App):
+	
 	def build(self):
 		Window.clearcolor = (1, 0, 0, 1)
 		return sm
