60087037e178618e719e91de03fe52915b811177
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2019-05-28 13:54:32 -0400

    Condensed Hangman.py
    What it says above. Just deleted unnecessary stuff

--- Hangman1.py
+++ Hangman1.py
@@ -1,41 +1,10 @@
 import random
-import sys
-from kivy.app import App
-from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import ObjectProperty
-from kivy.graphics import Color, Rectangle
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.core.window import Window
+
 class ManHangs():
 	prompt = """Hello my good sir or mam, or animal or alien or whatever you\nare. My name is Jordan, and we're gonna play hangman cus I suck\nat programming and only know how to make this game.\nI'm sure you know how to play it already though.\n"""
 	#	letter_representation()
 
-	ticks = []
-	ticks.append("""       
-							_____
-						   |     |
-								 |
-								 |
-								 |
-								 |
-				_________________|_________
-			""")
-	
-	def print_prompt(self):
-		w = Button(text = "Hii")
-		app.root.current.add_widget(w)
-	def func(self):
-		c = input("Would you like to continue? y/n\n")
-		if c == "n":
-			print("That's unfortunate...")
-			sys.exit(0)
-		else:
-			print("Let's get this show on the road then! ")
+
 
 	def selecting_hangman(self):
 		word_collection = ["soda", "izze", "compile", "build"] #collection of words the computer can choose from
@@ -48,69 +17,11 @@ class ManHangs():
 		return set_in
 	
 	def test_guessed_letter(self, guess, secret_word, count):
-		
-		definitions = {1: "first", 2:"second", 3:"third", 4:"forth"}
-		
 		if guess not in secret_word: #if guessed is wrong, add to counter
 			count += 1
-			print("That was the " + definitions[count] + " time bro!")
-		if count == 0: #shows how close user is to losing
-			print("""       
-							_____
-						   |     |
-								 |
-								 |
-								 |
-								 |
-				_________________|_________
-			""")
-		elif count == 1:
-			print("""       
-							_____
-						   |     |
-						   O     |
-								 |
-								 |
-								 |
-				_________________|_________
-			""")
-		elif count == 2:
-			print("""       
-							_____
-						   |     |
-						   o     |
-						   |     |
-								 |
-								 |
-				_________________|_________
-			""")
-		elif count == 3:
-			print("""       
-							_____
-						   |     |
-						   o     |
-						  /|\    |
-								 |
-								 |
-				_________________|_________
-			""")
-		elif count == 4:
-			print("""       
-							_____
-						   |     |
-						   o     |
-						  /|\    |
-						  / \     |
-								 |
-				_________________|_________
-			""")
 		return count
 
 	def nitty_gritty(self, hangman, guessed_letter, guessed_letters):
-		print("Al the nitty gritty is under this: \n")
-		print(hangman)
-		print(guessed_letter)
-		print(guessed_letters)
 		if guessed_letters == []:
 			set_in = self.letter_representation(hangman)
 		else:
@@ -125,48 +36,6 @@ class ManHangs():
 					elif (letter != guessed_letter) and (j == len(guessed_letters)):
 						set_in += " _"
 					j += 1
-			print("This is set_in: ")
-			print(set_in)
 		return set_in
 
 
-def main():
-
-	active_2 = True
-	while active_2:
-		prompt_user()
-		guessed_letters = [] #sets up a bank of all the letters the user has tried
-		active = True #flag
-		counter = 0
-		while active: #main game
-			 #higher the counter goes, the closer you are to losing, passed tto test letter
-			
-			guessed_letter = input("Guess a letter: ") #asks the user for a letter
-			counter = test_guessed_letter(guess = guessed_letter, secret_word = hangman, count = counter)
-			#tests letter for certain things
-			if counter == 4:
-				a = input("You lose... Do you wanna play again?")
-				break
-			
-			guessed_letters.append(guessed_letter) #adds to bank of used letters
-
-			set_in = ""
-			for letter in hangman: # MOST DIFFICULT PART OF THE PROGRAM TO CONSTRUCT
-				j = 1
-				for guessed_letter in guessed_letters: #goes through each letter in bank
-					if letter == guessed_letter: 
-						set_in += guessed_letter
-						break
-					elif (letter != guessed_letter) and (j == len(guessed_letters)):
-						set_in += " _"
-					j += 1
-
-			print(set_in)
-			if set_in == hangman:
-				print("You won!!")
-				break
-		a = input("Do you wanna play again??:")
-		if a == "y":
-			continue
-		else:
-			break
--- main.py
+++ main.py
@@ -15,29 +15,29 @@ from kivy.core.window import Window
 
 
 Builder.load_file("yourpops.kv")
-class IDK():
-	x = mh()
 
-guessed_letters = []
-guessed_letter = ""
-count = 0
-hangman = IDK.x.selecting_hangman()
-deleted_buttons = []
+y = mh()
+guessed_letters = [] #bank of letters guessed, global variable
+guessed_letter = "" # keeps track of the last guessed letter
+count = 0 #how many times user has been wrong
+hangman = y.selecting_hangman() #randomly selects hangman
+deleted_buttons = [] #bank of letters used/deleted
 
-class CustomButton(Button,IDK): #blueprint for my special buttons that delete themselves on touch
+class CustomButton(Button): #blueprint for my special buttons that delete themselves on touch
     def on_touch_down(self, touch):
         global count
         global guessed_letters
         global guessed_letter
         global deleted_buttons
+        #all of these above will be used
         if self.collide_point(*touch.pos): #if touched
            # print(f"\nCustomButton.on_touch_down: text/id={self.id}")
             guessed_letter = self.id  #the guessed letter used for logic is the id of the button
             guessed_letters.append(guessed_letter) #add that letter to the list of used letters for logic
-            count =  IDK.x.test_guessed_letter(guessed_letter, hangman, count) #sees if it was right or wrong and adjusts count accordingly
-            self.parent.btn_rep.text = IDK.x.nitty_gritty(hangman, guessed_letter, guessed_letters) #the representation of all the letters guessed
+            count =  y.test_guessed_letter(guessed_letter, hangman, count) #sees if it was right or wrong and adjusts count accordingly
+            self.parent.btn_rep.text = y.nitty_gritty(hangman, guessed_letter, guessed_letters) #the representation of all the letters guessed
          
-            if '_' not in self.parent.btn_rep.text:
+            if '_' not in self.parent.btn_rep.text: #if all letters are filled out, go to the win screen
                 sm.current = "forth"
             
             if count == 1:
@@ -49,9 +49,10 @@ class CustomButton(Button,IDK): #blueprint for my special buttons that delete th
             elif count == 4:
                 self.parent.new_image.source = 'hangmanpic4.png'
                 sm.current = "fifth"
+                #these update the pic based on how many times they've been wrong
             if count != 4:
-                btn_info = {"text" : self.text, id : self.id, "pos_hint" : self.pos_hint, "size_hint": self.size_hint}
-                deleted_buttons.append(btn_info)
+                btn_info = {"text" : self.text, id : self.id, "pos_hint" : self.pos_hint, "size_hint": self.size_hint} #stores info of button in dictionary before destroying itself
+                deleted_buttons.append(btn_info) #adds the dictionary to a list
                 self.parent.remove_widget(self) #suicides itself
             return True    # consumed on_touch_down & stop propagation / bubbling
         return super(CustomButton, self).on_touch_down(touch)
@@ -61,17 +62,18 @@ class WindowManager(ScreenManager):
 	pass
 class MainWindow(Screen):
 	pass
-class SecondWindow(Screen,mh):
+class SecondWindow(Screen, mh):
 	pass
-class ThirdWindow(Screen, IDK):
+class ThirdWindow(Screen):
 	global hangman
 
 	def __init__(self, **kwargs):
 		super(ThirdWindow, self).__init__(**kwargs)
-		self.btn_rep = ""
-		self.new_image = ""
 		self.create_buttons()
-		
+		self.btn_rep =((Label(id = "btn_rep", text= "Secret Word", font_size = 30, color = (1,1,1,1), size_hint =  (.3, .3), pos_hint = {'x':.325-.15,'y':.2-.15})))
+		self.add_widget(self.btn_rep)
+		self.new_image = Image(source = 'hangmanpic0.png', size_hint = (.6,.7), pos_hint = {'x':.2, 'y': .32}, allow_stretch = True)
+		self.add_widget(self.new_image)		
 
 	def create_buttons(self): #properties of each button
 		k = .65
@@ -88,10 +90,7 @@ class ThirdWindow(Screen, IDK):
 				k = 0.65
 				c = 0	
 		
-		self.btn_rep =((Label(id = "btn_rep", text= "Secret Word", font_size = 30, color = (1,1,1,1), size_hint =  (.3, .3), pos_hint = {'x':.325-.15,'y':.2-.15})))
-		self.add_widget(self.btn_rep)
-		self.new_image = Image(source = 'hangmanpic0.png', size_hint = (.6,.7), pos_hint = {'x':.2, 'y': .32}, allow_stretch = True)
-		self.add_widget(self.new_image)
+
 		
 	def restore_buttons(self): 
 		global deleted_buttons
@@ -107,12 +106,14 @@ class ForthWindow(Screen):
 		global guessed_letter 
 		global hangman  
 		global count
-		hangman = IDK.x.selecting_hangman()
+		hangman = y.selecting_hangman()
 		count = 0
 		guessed_letter = ""
 		guessed_letters = []
 		s3 = self.manager.get_screen("third")
 		s3.restore_buttons()
+		s3.btn_rep.text = "Secret Word" 
+		s3.new_image.source = "hangmanpic0.png"
 		
 	
 class FifthWindow(Screen):
@@ -121,12 +122,14 @@ class FifthWindow(Screen):
 		global guessed_letter 
 		global hangman  
 		global count
-		hangman = IDK.x.selecting_hangman()
+		hangman = y.selecting_hangman()
 		count = 0
 		guessed_letter = ""
 		guessed_letters = []
 		s3 = self.manager.get_screen("third")
-		s3.restore_buttons() 
+		s3.restore_buttons()
+		s3.btn_rep.text = "Secret Word" 
+		s3.new_image.source = "hangmanpic0.png"
 		
 	
 sm = WindowManager()
